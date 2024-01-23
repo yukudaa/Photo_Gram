a8fccaa6055e973216bb1cf15eea6bc97470e48c
sure <sure@->
2024-01-23 11:25:40 +0900

    24_01_23 회원 정보 수정

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,4 +1,26 @@
 // (1) 회원정보 수정
-function update() {
+function update(userId, event) {
+	event.preventDefault(); // 폼태크 액샨을 막기!!
+
+	let data = $("#profileUpdate").serialize();
+
+	console.log(data);
+
+	$.ajax({
+		type: "put",
+		url: '/api/user/${userId}',
+		data: data,
+		contentType: "application/x-www-form-urlencoded;charset=utf-8",
+		dataType: "json"
+	}).done(res => { // HttpStatus 상태코드 200번대
+		console.log("update 성공");
+		location.href = '/user/${userId}'; // 수정하면 메인으로
+	}).fail(error => { // HttpStatus 상태코드 200번대 아닐 때
+		if (error.data == null) {
+			alert(error.responseJSON.message);
+		} else {
+			alert(JSON.stringify(error.responseJSON.data));
+		}
+	});
 
 }
