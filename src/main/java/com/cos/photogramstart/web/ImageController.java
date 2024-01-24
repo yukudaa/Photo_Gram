e7fce5b86ba18d54770553164de1c250f91eee66
yukudaa <leeyookoo@naver.com>
2024-01-24 23:21:08 +0900

    24_01_24 프로필 페이지

--- src/main/java/com/cos/photogramstart/web/ImageController.java
+++ src/main/java/com/cos/photogramstart/web/ImageController.java
@@ -1,10 +1,22 @@
 package com.cos.photogramstart.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.cos.photogramstart.config.auth.PrincipalDatails;
+import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.service.ImageService;
+import com.cos.photogramstart.web.dto.image.ImageUploadDto;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
-public class ImageController {
+@RequiredArgsConstructor
+public class ImageController { 
+	
+	private final ImageService imageService;
 
 	@GetMapping({"/","/image/story"})
 	public String story() {
@@ -19,5 +31,19 @@ public class ImageController {
 	@GetMapping({"/image/upload"})
 	public String upload() {
 		return "image/upload";
+	}
+	
+	// 사용자에게 데이터를 받고 service에게 호출 해주면 된다.
+	@PostMapping("/image")
+	public String imageUpload(ImageUploadDto imageUploadDto, @AuthenticationPrincipal PrincipalDatails principalDatails) {
+		
+		if(imageUploadDto.getFile().isEmpty()) {
+			throw new CustomValidationException("이미지가 첨부되지 않았습니다.",null);
+		}
+		
+		// 서비스 호출
+		imageService.사진업로드(imageUploadDto, principalDatails);
+		
+		return "redirect:/user/"+principalDatails.getUser().getId(); // 업로드를 딱 누르면 /user/{?}로 오게
 	}
 }
