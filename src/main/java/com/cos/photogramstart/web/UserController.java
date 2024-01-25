206b0e3e0e3bb389ceefdc18d261dd1cc046098b
yukudaa <leeyookoo@naver.com>
2024-01-25 20:03:13 +0900

    24_01_25 프로필 페이지

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.cos.photogramstart.config.auth.PrincipalDatails;
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.service.UserService;
+import com.cos.photogramstart.web.dto.user.UserProfileDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,23 +21,22 @@ public class UserController {
 
 	private final UserService userService;
 	
-	
-	@GetMapping("/user/{id}")
-	public String profile(@PathVariable int id, Model model) {
-		User userEntity = userService.회원프로필(id);
-		model.addAttribute("user",userEntity);
+	@GetMapping("/user/{pageUserId}")
+	public String profile(@PathVariable int pageUserId, Model model,@AuthenticationPrincipal PrincipalDatails principalDatails) {
+		UserProfileDto dto = userService.회원프로필(pageUserId, principalDatails.getUser().getId());
+		model.addAttribute("dto",dto);
 		return "user/profile";
 	}
 	
 	@GetMapping("/user/{id}/update")
 	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDatails principalDatails) {
 		// 1. 추천
-		System.out.println("세션 정보 : "+principalDatails.getUser());
+		// System.out.println("세션 정보 : "+principalDatails.getUser());
 		
 		// 2. 극혐
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication(); 
 		PrincipalDatails mPrincipalDatails = (PrincipalDatails) auth.getPrincipal();
-		System.out.println("직접 찾은 세션 정보: " + mPrincipalDatails.getUser()); // 최악
+		// System.out.println("직접 찾은 세션 정보: " + mPrincipalDatails.getUser()); // 최악
 		
 		return "user/update";
 	}
