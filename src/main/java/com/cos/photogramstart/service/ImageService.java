bf91f7ae97639914d5e8e6f2548c3ca0918f928c
leeyookoo <leeyookoo@naver.com>
2024-01-29 16:04:11 +0900

    24_01_29 기타

--- src/main/java/com/cos/photogramstart/service/ImageService.java
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -25,6 +25,12 @@ public class ImageService {
 
 	private final ImageRepository imageRepository;
 	
+	@Transactional(readOnly = true)
+	public List<Image> 인기사진() {
+		return imageRepository.mPopular();
+	}
+	
+	
 	@Transactional(readOnly = true) // 영속성 컨텍스트 변경 감지를 해서, 더티체킹, flush(반영) X
 	public Page<Image> 이미지스토리(int principalId,Pageable pageable) {
 		Page<Image> images = imageRepository.mStory(principalId, pageable);
--- src/main/java/com/cos/photogramstart/service/UserService.java
+++ src/main/java/com/cos/photogramstart/service/UserService.java
@@ -1,14 +1,21 @@
 package com.cos.photogramstart.service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 import java.util.function.Supplier;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
+import com.cos.photogramstart.handler.ex.CustomApiException;
 import com.cos.photogramstart.handler.ex.CustomException;
 import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.web.dto.user.UserProfileDto;
@@ -22,6 +29,32 @@ public class UserService {
 	private final UserRepository userRepository;
 	private final SubscribeRepository subscribeRepository;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Value("${file.path}")
+	private String uploadFolder; // application.yml의 값 가져오기
+	
+	@Transactional
+	public User 회원프로필사진변경(int principalId, MultipartFile profileImageFile) {
+		UUID uuid = UUID.randomUUID(); // uuid
+		String imageFileName = uuid+"_"+profileImageFile.getOriginalFilename(); // 실제 file 이름이 들어감 1.jpg
+		System.out.println("이미지 파일이름: "+imageFileName);
+		
+		Path imageFilePath = Paths.get(uploadFolder+imageFileName); // 경로 + file명
+		
+		// 통신, I/O -> 예외가 발생할 수 있다.
+		try {
+			Files.write(imageFilePath, profileImageFile.getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		User userEntity = userRepository.findById(principalId).orElseThrow(() ->{
+			throw new CustomApiException("유저를 찾을 수 없습니다.");
+		});
+		userEntity.setProfileImageUrl(imageFileName);
+		
+		return userEntity;
+	} // 더티체킹으로 업데이트 됨.
 	
 	@Transactional(readOnly=true) // select는 readOnly = true
 	public UserProfileDto 회원프로필(int pageUserId, int principalId) {
@@ -42,6 +75,11 @@ public class UserService {
 		
 		dto.setSubscribeCount(subscribeCount);
 		dto.setSubscribeState(subscribeState == 1);
+		
+		// 좋아요 카운트 추가하기
+		userEntity.getImages().forEach((image)->{
+			image.setLikeCount(image.getLikes().size());
+		});
 		
 		return dto;
 	}
